25896ea48c0467a0f9bbeb576d83fd547da1e909
jttc98 <jttc98@csu.fullerton.edu>
2018-02-21 20:08:03 -0800

    Update Registrar.h

--- Registrar.h
+++ Registrar.h
@@ -7,7 +7,8 @@ using namespace std;
 
 class Registrar {
 public:
-
+	Registrar();	// empty constructor
+	~Registrar();	// destructor
 	void readTextfile(string filename); // Load information from a text file with the given filename: THIS IS COMPLETE
 	void addLine(string courseName, string cwid, char grade); // process a line from the text file
 	Student& getStudent(string cwid) const; // return the Student object corresponding to a given CWID
